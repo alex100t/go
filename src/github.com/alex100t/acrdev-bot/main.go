b466eedb64d4600bd84b277f605721a7241cbd88
Alex Butylkin <butylkin.aleksander@otr.ru>
2021-12-18 15:13:36 +0300

    + insert and update

--- src/github.com/alex100t/acrdev-bot/main.go
+++ src/github.com/alex100t/acrdev-bot/main.go
@@ -10,18 +10,19 @@ import (
 const (
 	host     = "localhost" // Точка входа
 	port     = 5432
-	user     = "pguser" // Пользователь БД
-	dbname   = "db1"    // Идентификатор подключения
+	user     = "pguser"   // Пользователь БД
+	dbname   = "postgres" // Идентификатор подключения
 	password = "zzz"
 )
 
-type Response struct {
-	StatusCode int         `json:"statusCode"`
-	Body       interface{} `json:"body"`
+type recinfo struct {
+	description string
 }
 
 func main() {
 	//psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	fmt.Println("Connect to database...")
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
@@ -36,46 +37,62 @@ func main() {
 		panic(err)
 	}
 
-	_, err = db.Query("select 1")
-	if err != nil {
-		panic(err)
-	}
-}
-
-/*
-// Получение IAM-токена сервисного аккаунта, указанного в настройках функции
-func getToken(ctx context.Context) string {
-	resp, err := ycsdk.InstanceServiceAccount().IAMToken(ctx)
-	if err != nil {
-		panic(err)
-	}
-	return resp.IamToken
-}
-
-
-// Подключение к БД
-func Handler(ctx context.Context) (*Response, error) {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=require",
-		host, port, user, getToken(ctx), dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("connected")
 
 	_, err = db.Query("select 1")
 	if err != nil {
 		panic(err)
 	}
 
-	return &Response{
-		StatusCode: 200,
-		Body:       "Successfully connected!",
-	}, nil
+	fmt.Println("Connection valid")
+
+	fmt.Println("Get existed record:")
+
+	acr := "ДРИБС"
+	row := db.QueryRow("select description from acronyms where acronym = $1", acr)
+	if err != nil {
+		panic(err)
+	}
+	//defer row.Close()
+	record := recinfo{}
+
+	err = row.Scan(&record.description)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(record.description)
+
+	fmt.Println("Inser new record")
+	result, err := db.Exec("insert into acronyms (acronym, description) values ($1, $2)",
+		"ЗЗЗ", "Тестовый акроним")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.LastInsertId()) // не поддерживается
+	fmt.Println(result.RowsAffected()) // количество добавленных строк
+
+	fmt.Println("Select new record:")
+	acr = "ЗЗЗ"
+	row = db.QueryRow("select description from acronyms where acronym = $1", acr)
+	if err != nil {
+		panic(err)
+	}
+	//defer row.Close()
+	record = recinfo{}
+
+	err = row.Scan(&record.description)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(record.description)
+
+	fmt.Println("Delete new record:")
+	result, err = db.Exec("delete from acronyms where acronym = $1", acr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.RowsAffected()) // количество удаленных строк
+
 }
-*/
